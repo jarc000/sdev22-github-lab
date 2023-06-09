e8283d03ceea948ee0a6b24050acf4d04d2ed60d
Juno Arciaga <junoarciaga1@gmail.com>
2023-06-09 00:27:32 -0700

    Added documentation to the project.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-public class Main {
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author Juno Arciaga
+ * @version 1.0
+ */
+
+public class Main
+{
+    /**
+     * Starts the application
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world,  from SDEV 220!");
